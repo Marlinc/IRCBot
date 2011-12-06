0608134891b240e63ad56342978b642bf14d21f4
Marlin Cremers <marlin.cremers@gmail.co>
2011-12-06 19:07:03 +0100

    Updated the identifier handler to allow a array of options

--- src/Handlers/Identifiers.php
+++ src/Handlers/Identifiers.php
@@ -4,7 +4,16 @@ class IRCBot_Handlers_Identifiers
     private $_identifiers = array();
     public function set($identifier, $value)
     {
-        $this->_identifiers[$identifier] = $value;
+        if (is_array(func_get_arg(0)) {
+            $settings = func_get_arg(0);
+            foreach ($settings as $setting => $value) {
+                $this->set($setting, $value);
+            }
+        } else {
+            $identifier = func_get_arg(0);
+            $value = func_get_arg(1);
+            $this->_identifiers[$identifier] = $value;
+        }
         return $this;
     }
     public function  __set($identifier, $value)
