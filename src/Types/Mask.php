303fcc8b4d4876119a18514840f0e15965b2bd47
Marlin Cremers <marlin.cremers@gmail.co>
2011-12-10 13:58:46 +0100

    Made it more easier to use the Mask class

--- src/Types/Mask.php
+++ src/Types/Mask.php
@@ -4,11 +4,19 @@ class IRCBot_Types_Mask
     public $nickname;
     public $ident;
     public $host;
+    
+    public function __construct($mask = null)
+    {
+        if ($mask) {
+            $this->fromMask($mask);
+        }
+    }
     public function fromMask($mask)
     {
         sscanf($mask, '%[^!]!%[^@]@%s', $this->nickname, $this->ident, $this->host);
     }
-    public function  __toString() {
+    public function  __toString()
+    {
         return sprintf('%s!%s@%s', $this->nickname, $this->ident, $this->host);
     }
 }
